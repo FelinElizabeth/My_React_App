10b1f06a17d0d9af9740f6a9650284430361cfce
FelinElizabeth <felinelizabethbinoy@gmail.com>
2023-05-30 23:51:46 +0530

    Updates

---
+++ precache-manifest.03e2487fc3f168888339371dcf1c1bbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fbf223c3359ba3500c2",
+    "url": "/My_React_App/static/css/main.a919f2e6.chunk.css"
+  },
+  {
+    "revision": "6fbf223c3359ba3500c2",
+    "url": "/My_React_App/static/js/main.6fbf223c.chunk.js"
+  },
+  {
+    "revision": "2ce1ffecb0cafd39e3f3",
+    "url": "/My_React_App/static/js/1.2ce1ffec.chunk.js"
+  },
+  {
+    "revision": "18469ebba1ca7209ca56",
+    "url": "/My_React_App/static/js/runtime~main.18469ebb.js"
+  },
+  {
+    "revision": "0d1dc6ef62ce301f47c6310c1400e616",
+    "url": "/My_React_App/index.html"
+  }
+];
